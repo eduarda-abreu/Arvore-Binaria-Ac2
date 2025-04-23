d3e11e0b8847c78559b70dfb936e11d332615bcb
Eduarda Abreu <gabreududa@gmail.com>
2025-04-23 20:06:16 -0300

    Definindo as structs do prontuário

--- arvorebinaria.h
+++ arvorebinaria.h
@@ -1,3 +1,16 @@
 /*  Esqueleto para a função de Arvore Binária de Busca
     Objetivo - Criar Arvore Binária de Busca para buscar prontuarios médicos a partir de cpf (chave que não repete)
 */
+
+typedef struct {
+    int data, mes, ano;
+}Data;
+
+
+typedef struct 
+{
+    char nome[100];
+    char cpf[15];
+    Data dataNasc; 
+    char historico[1000];
+}Prontuario;
