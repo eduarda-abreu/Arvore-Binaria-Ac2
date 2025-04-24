8942a7bac6536ae342712bcf4d900fde6b504c10
Eduarda Abreu <gabreududa@gmail.com>
2025-04-23 22:07:33 -0300

    Iniciei as implementações dos protótipos

--- arvorebinaria.c
+++ arvorebinaria.c
@@ -1,1 +1,9 @@
-//Implementação das funções do esqueleto
+//Implementação do esqueleto da Arvore binária
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "arvorebinaria.h"
+
+void inicializarArvore(ArvB *arv){
+    arv->raiz = NULL;
+}
